e7f0c6ea4915de47137c894c0d24785adeb0f1e8
Kevin Vitale <kevinvitale@gmail.com>
2016-07-29 12:07:40 -0400

    [Misc] minor doc additions

--- Source/ResourceJSON.swift
+++ Source/ResourceJSON.swift
@@ -9,6 +9,7 @@ public protocol ResourceJSON {
      Parses the receiver from the given `json`, or returns `nil` if parsing faile.
      
      - parameter json: A JSON object.
+     - returns: An instance of `Self`, or `nil`.
      */
     init?(_ json: [String:AnyObject])
 }
