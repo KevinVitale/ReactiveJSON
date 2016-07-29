dad646d360ece8dd17338db2c276a58d2ce0d7d7
Kevin Vitale <kevinvitale@gmail.com>
2016-07-29 13:16:08 -0400

    [Network] `JSONService` can parse endpoint resources

--- Source/JSONService.swift
+++ Source/JSONService.swift
@@ -21,7 +21,7 @@ extension JSONService {
      - parameter method: How the request should be made. Defaults to `GET`.
      - parameter parameters: Optional request parameters.
      
-     - returns: A signal that, when started, sends `T` values parsed from response, or an error.
+     - returns: A producer that, when started, sends `T` values parsed from response, or an error.
      */
     public static func request<T>(endpoint endpoint: String, method: RequestMethodType = .Get, parameters: [String:AnyObject]? = nil, token: AuthToken = .None) -> SignalProducer<T, NetworkError> {
         return sharedInstance().request(endpoint: endpoint, method: method, parameters: parameters, token: token)
@@ -34,9 +34,22 @@ extension JSONService {
      - parameter method: How the request should be made. Defaults to `GET`.
      - parameter parameters: Optional request parameters.
      
-     - returns: A signal that, when started, sends `R` values parsed from response, or an error.
+     - returns: A producer that, when started, sends `R` values parsed from response, or an error.
      */
     public static func request<R: ResourceJSON>(endpoint endpoint: String, method: RequestMethodType = .Get, parameters: [String : AnyObject]? = nil, token: AuthToken = .None) -> SignalProducer<R, NetworkError> {
         return request(endpoint: endpoint, method: method, parameters: parameters, token: token).mapResourceJSON()
+    }
+
+    /**
+     Performs a JSON request, attempting to map the result to an instance of `EndpointResource<R>`.
+
+     - parameter endpoint: The endpoint string.
+     - parameter method: How the request should be made. Defaults to `GET`.
+     - parameter parameters: Optional request parameters.
+     
+     - returns: A producer that, when started, sends an optional `EndpointResource` of type `R`, or an error.
+     */
+    public static func request<R: Resourceable>(endpoint endpoint: String, method: RequestMethodType = .Get, parameters: [String : AnyObject]? = nil, token: AuthToken = .None) -> SignalProducer<EndpointResource<R>?, NetworkError> {
+        return request(endpoint: endpoint, method: method, parameters: parameters, token: token).map(R.parse)
     }
 }
