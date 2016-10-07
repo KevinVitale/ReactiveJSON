380f2d9344b65de485799db772e90f762b375404
Kevin Vitale <kevinvitale@gmail.com>
2016-10-07 13:04:34 -0400

    [Misc] moves 'Identity' into its own source file

--- Source/Protocols.swift
+++ Source/Protocols.swift
@@ -1,34 +1,6 @@
 import ReactiveCocoa
 import Result
 
-// MARK: - Identity -
-//------------------------------------------------------------------------------
-public protocol Identity {
-    /// The identity's `TYPE` value.
-    associatedtype Value: Hashable
-
-    /// - parameter id: A unique identifying value.
-    var id: Value? { get }
-}
-
-// MARK: - Int, Identity -
-//------------------------------------------------------------------------------
-extension Int: Identity {
-    public typealias Value = Int
-    public var id: Value? {
-        return self
-    }
-}
-
-// MARK: - String, Identity -
-//------------------------------------------------------------------------------
-extension String: Identity {
-    public typealias Value = String
-    public var id: Value? {
-        return self
-    }
-}
-
 // MARK: - JSON Service -
 //------------------------------------------------------------------------------
 extension Singleton where Instance: ServiceHost {
